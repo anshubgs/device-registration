ce69cb8f3cc15e540405cbebbb431eae469a697a
Anshu kumar <gautamanshu285@gmail.com>
2026-01-21 23:42:53 +0530

    Initial commit

---
+++ src/main/java/com/anshu/device_registration/DeviceRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.anshu.device_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anshu/device_registration/DeviceRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshu.device_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
